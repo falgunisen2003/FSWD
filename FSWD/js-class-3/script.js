f93478446cd922ed654ebbdd629dd3c7440fff17
falgunisen2003 <falgunisen0@gmail.com>
2025-02-26 03:57:16 +0530

    5th commit

---
+++ FSWD/js-class-3/script.js
@@ -0,0 +1,218 @@
+
+
+// // // var a=10
+// // // console.log(a,typeof(a));
+// // // console.log(a,typeof(a),typeof(typeof(a)));
+
+// // // // var b=String(a)
+// // // // console.log(b,typeof(b));
+
+// // // var name="poll"
+
+// // // // console.log(`welcome to bengal coding acdemy ${name},hoe are you ${name}`);
+
+// // // console.log(0/0);
+// // // console.log(undefined);
+// // // console.log(typeof(null));
+// // // console.log(typeof(NaN));
+// // // console.log(isNaN(100));
+// // // console.log(isNaN("100d"));
+// // // console.log(isNaN("d"));
+
+// // let str="bengal coding academy";
+// // console.log(str.charAt(str.length));
+// // console.log(str.charAt(str.length -1));
+// // // "length" => this is attribute
+
+// // let a="hello "
+// // let b="world"
+// // // let c=a.concat(b);
+// // let c=b.concat(a);
+// // console.log(c);
+
+// // console.log(a.indexOf("l"));
+// // console.log(a.lastIndexOf("l"));
+// // // array
+// //  let arr=[10,40,[9,8,6],56,"hlw world",true,false]
+// // // console.log(arr[4]);
+// // // console.log(arr[2][1]);
+
+// // // 2d array
+// // let arr1=[[1, 2, 3],
+// //           [4, 5, 6],
+// //           [7, 8, 9]];
+
+// //  console.log(arr1[1][1]);
+// //  console.log(arr1[2][2]);
+
+// // // 2.object
+// // let obj={
+// //     name: "bca",
+// //     age:2,
+// //     courses:["fswd","c","dsa"],
+// //     location:"kolcation",
+// //     isVirtual: true,
+// // };
+// // console.log(obj.name);
+// // console.log(obj.courses);
+
+// // //slice,splice
+
+
+// // var a=10
+// // console.log(a,typeof(a));
+// // console.log(a,typeof(a),typeof(typeof(a)));
+
+// // // var b=String(a)
+// // // console.log(b,typeof(b));
+
+// // var name="poll"
+
+// // // console.log(`welcome to bengal coding acdemy ${name},hoe are you ${name}`);
+
+// // console.log(0/0);
+// // console.log(undefined);
+// // console.log(typeof(null));
+// // console.log(typeof(NaN));
+// // console.log(isNaN(100));
+// // console.log(isNaN("100d"));
+// // console.log(isNaN("d"));
+
+// let str="bengal coding academy";
+// console.log(str.charAt(str.length));
+// console.log(str.charAt(str.length -1));
+// // "length" => this is attribute
+
+// let a="hello "
+// let b="world"
+// // let c=a.concat(b);
+// let c=b.concat(a);
+// console.log(c);
+
+// console.log(a.indexOf("l"));
+// console.log(a.lastIndexOf("l"));
+// // array
+//  let arr=[10,40,[9,8,6],56,"hlw world",true,false]
+// // console.log(arr[4]);
+// // console.log(arr[2][1]);
+
+// // 2d array
+// let arr1=[[1, 2, 3],
+//           [4, 5, 6],
+//           [7, 8, 9]];
+
+//  console.log(arr1[1][1]);
+//  console.log(arr1[2][2]);
+
+// // 2.object
+// let obj={
+//     name: "bca",
+//     age:2,
+//     courses:["fswd","c","dsa"],
+//     location:"kolcation",
+//     isVirtual: true,
+// };
+// console.log(obj.name);
+// console.log(obj.courses);
+
+// //slice,splice
+
+
+
+// // var a=10
+// // console.log(a,typeof(a));
+// // console.log(a,typeof(a),typeof(typeof(a)));
+
+// // // var b=String(a)
+// // // console.log(b,typeof(b));
+
+// // var name="poll"
+
+// // // console.log(`welcome to bengal coding acdemy ${name},hoe are you ${name}`);
+
+// // console.log(0/0);
+// // console.log(undefined);
+// // console.log(typeof(null));
+// // console.log(typeof(NaN));
+// // console.log(isNaN(100));
+// // console.log(isNaN("100d"));
+// // console.log(isNaN("d"));
+
+// let str="bengal coding academy";
+// console.log(str.charAt(str.length));
+// console.log(str.charAt(str.length -1));
+// // "length" => this is attribute
+
+// let a="hello "
+// let b="world"
+// // let c=a.concat(b);
+// let c=b.concat(a);
+// console.log(c);
+
+// console.log(a.indexOf("l"));
+// console.log(a.lastIndexOf("l"));
+// // array
+//  let arr=[10,40,[9,8,6],56,"hlw world",true,false]
+// // console.log(arr[4]);
+// // console.log(arr[2][1]);
+
+// // 2d array
+// let arr1=[[1, 2, 3],
+//           [4, 5, 6],
+//           [7, 8, 9]];
+
+//  console.log(arr1[1][1]);
+//  console.log(arr1[2][2]);
+
+// // 2.object
+// let obj={
+//     name: "bca",
+//     age:2,
+//     courses:["fswd","c","dsa"],
+//     location:"kolcation",
+//     isVirtual: true,
+// };
+// console.log(obj.name);
+// console.log(obj.courses);
+
+// //slice,splice
+
+
+// let a=18<20
+// let b=18>20
+
+// console.log(a);
+// console.log(b);
+// let b=76;
+// c=56;
+// console.log(c>b);
+
+
+// if(899){
+//     console.log("i am from if block");
+    
+
+// }
+// else{
+//     console.log("i am from else block");
+    
+// }
+
+
+// for loop
+let a=5;
+while(a>=0){
+console.log(a);
+a=a-1;
+
+}
+
+
+
+
+
+
+
+
+
+
--- FSWD/script.js
+++
@@ -1,5 +0,0 @@
-// console.log("hello")
-var a=10
-console.log(a,typeof(a));
-var b=String(a)
-console.log(b,typeof(b));
