1007dd977a2dee446650b8bdf68b818e6dbe390b
falgunisen2003 <falgunisen0@gmail.com>
2025-03-02 01:28:11 +0530

    7th commit

--- FSWD/js-class-3/script.js
+++ FSWD/js-class-3/script.js
@@ -1,628 +1,328 @@
+// ----------------------------DOM--------------------------------
+// getElementbyId(),
+// grtElementsByclass(),
+// getelementsbyTagname(),
+// QuerySelector(),
+
+// -------change the color bg and text---------------
+// let h1 = document.getElementById('container-h1');
+// h1.style.backgroundColor= 'blue';
+// // let btn = document.getElementById('button');
+// function changeColor(){
+//     h1.style.backgroundColor= 'yellow';
+//     h1.style.color='red';
+// }
 
 
-// // // var a=10
-// // // console.log(a,typeof(a));
-// // // console.log(a,typeof(a),typeof(typeof(a)));
-
-// // // // var b=String(a)
-// // // // console.log(b,typeof(b));
-
-// // // var name="poll"
-
-// // // // console.log(`welcome to bengal coding acdemy ${name},hoe are you ${name}`);
-
-// // // console.log(0/0);
-// // // console.log(undefined);
-// // // console.log(typeof(null));
-// // // console.log(typeof(NaN));
-// // // console.log(isNaN(100));
-// // // console.log(isNaN("100d"));
-// // // console.log(isNaN("d"));
-
-// // let str="bengal coding academy";
-// // console.log(str.charAt(str.length));
-// // console.log(str.charAt(str.length -1));
-// // // "length" => this is attribute
-
-// // let a="hello "
-// // let b="world"
-// // // let c=a.concat(b);
-// // let c=b.concat(a);
-// // console.log(c);
-
-// // console.log(a.indexOf("l"));
-// // console.log(a.lastIndexOf("l"));
-// // // array
-// //  let arr=[10,40,[9,8,6],56,"hlw world",true,false]
-// // // console.log(arr[4]);
-// // // console.log(arr[2][1]);
-
-// // // 2d array
-// // let arr1=[[1, 2, 3],
-// //           [4, 5, 6],
-// //           [7, 8, 9]];
-
-// //  console.log(arr1[1][1]);
-// //  console.log(arr1[2][2]);
-
-// // // 2.object
-// // let obj={
-// //     name: "bca",
-// //     age:2,
-// //     courses:["fswd","c","dsa"],
-// //     location:"kolcation",
-// //     isVirtual: true,
-// // };
-// // console.log(obj.name);
-// // console.log(obj.courses);
-
-// // //slice,splice
+// let container = document.getElementsByClassName('container')[0];
+// let containerH2=document.createElement('h2');
+// containerH2.innerText="learn coding!";
+// containerH2.className="container-h2";
+// container.appendChild(containerH2);
+// containerH2.style.backgroundColor='green';
+// containerH2.style.cursor='pointer';
 
 
-// // var a=10
-// // console.log(a,typeof(a));
-// // console.log(a,typeof(a),typeof(typeof(a)));
 
-// // // var b=String(a)
-// // // console.log(b,typeof(b));
 
-// // var name="poll"
 
-// // // console.log(`welcome to bengal coding acdemy ${name},hoe are you ${name}`);
 
-// // console.log(0/0);
-// // console.log(undefined);
-// // console.log(typeof(null));
-// // console.log(typeof(NaN));
-// // console.log(isNaN(100));
-// // console.log(isNaN("100d"));
-// // console.log(isNaN("d"));
 
-// let str="bengal coding academy";
-// console.log(str.charAt(str.length));
-// console.log(str.charAt(str.length -1));
-// // "length" => this is attribute
 
-// let a="hello "
-// let b="world"
-// // let c=a.concat(b);
-// let c=b.concat(a);
-// console.log(c);
 
-// console.log(a.indexOf("l"));
-// console.log(a.lastIndexOf("l"));
-// // array
-//  let arr=[10,40,[9,8,6],56,"hlw world",true,false]
-// // console.log(arr[4]);
-// // console.log(arr[2][1]);
 
-// // 2d array
-// let arr1=[[1, 2, 3],
-//           [4, 5, 6],
-//           [7, 8, 9]];
 
-//  console.log(arr1[1][1]);
-//  console.log(arr1[2][2]);
 
-// // 2.object
-// let obj={
-//     name: "bca",
-//     age:2,
-//     courses:["fswd","c","dsa"],
-//     location:"kolcation",
-//     isVirtual: true,
+
+
+
+
+
+
+
+
+// let h1 = document.getElementById('container-h1');
+
+// h1.style.backgroundColor = 'blue';
+
+// let container = document.getElementsByClassName('container')[0];
+
+// // let containerH2 = document.createElement('h2');
+// // containerH2.innerText = "Learn Coding!";
+// // containerH2.className = "container-h2";
+// // containerH2.id = "container-id";
+// // container.appendChild(containerH2);
+
+// // containerH2.style.cursor = "pointer";
+
+
+
+
+
+// // let btn = document.getElementById('button');
+
+// // function changeColor(){
+// //     h1.style.color = 'blue';
+// //     h1.style.backgroundColor = 'aquamarine';
+// // }
+
+// // let state = false;
+
+// // function alertMe(){
+// //     state = !state;
+
+// //     if(state){
+// //         h1.style.color = 'blue';
+// //         h1.style.backgroundColor = 'aquamarine';
+// //     }
+// //     else{
+// //         h1.style.color = 'black';
+// //         h1.style.backgroundColor = 'blue';
+// //     }
+
+// // }
+
+
+// function changeColorToPink(){
+//     document.getElementById('box-1').style.backgroundColor = "pink"
+// }
+// function changeColorToOrange(){
+//     document.getElementById('box-2').style.backgroundColor = "orange"
+
+
+
+// }
+// function changeColorToGreen(){
+//     document.getElementById('box-3').style.backgroundColor = "green"
+
+
+
+// }
+// function changeColorToBlue(){
+//     document.getElementById('box-4').style.backgroundColor = "blue"
+
+
+
+// }
+// function changeColorToRed(){
+//     document.getElementById('box-5').style.backgroundColor = "red"
+
+
+
+// }
+// function changeColorToYellow(){
+//     document.getElementById('box-6').style.backgroundColor = "yellow"
+
+
+
+// }
+
+// function callBackFn(){
+//     console.log("I am a call back function");
+    
+// }
+
+// setTimeout(callBackFn, 3000);
+
+// console.log("Hello")
+
+
+// setInterval(callBackFn, 3000)
+
+
+// setTimeout(()=>{
+//     console.log("Good Morning!");
+// }, 4000)
+
+// // setInterval(() => {
+//     console.log("Very Good Morning");
+// }, 2000);
+
+
+// -------------------example 1--------------------------
+// function callMe(b){
+//     let a=20;
+//     let func=()=>{
+//         console.log(a+b);
+        
+//     }
+// b++;
+// a++;
+// func();
+
+// }
+// let a=3;
+// callMe(a+2);
+
+// ----------------example 2---------------------------
+// function callMe(b){
+//     let a=20;
+//     let func=()=>{
+//         console.log(a+b);
+        
+//     }
+// setTimeout(func,3000);
+// b++;
+// a++;
+
+
+// }
+// let a=3;
+// callMe(a+2);
+
+    //    orrrrrrrrrrrrrr
+
+    // function callMe(b){
+    //     let a=20;
+        
+    // setTimeout(()=>{
+        
+    //         console.log(a+b);
+            
+    //     },3000)
+    
+    // b++;
+    // a++;
+    
+    
+    // }
+    // let a=3;
+    // callMe(a+2);
+
+    // -------------example 3--------------------
+    // function callMe(b){
+    //     let a=20;
+    //     let func=()=>{
+    //         console.log("3");
+    //         console.log(a+b);
+            
+            
+    //     }
+    //     console.log("1");
+    //     b++;
+    //     console.log("2");
+    //     setTimeout(func,3000);
+    //     console.log("4");
+    //     a++;
+    // }
+    // let a=3;
+    // callMe(a+2);
+
+    // -------------example 4-----------------
+    // console.log("0");
+    // queueMicrotask(()=>{
+    //     console.log("1");
+        
+    // })
+    // console.log("2");
+
+    // 0rrrrrrrrrrrrrrrrrrrr
+
+    // console.log("0");
+
+    // setTimeout(()=>{
+    //     console.log(4);
+        
+    // },0);
+    // queueMicrotask(()=>{
+    //     console.log("1");
+        
+    // })
+    // console.log("2");
+
+    // // ------promise object create-----------------------------
+    
+    // let p=new Object(); //create object
+
+    // class Prom{
+
+//     constructor(executor){
+//         executor(a, b)
+//     }
+
+//     let executor = (a, b)=>{
+//         if(){
+//             a()
+//         }
+//         {
+//             b()
+//         }
+//     }
+
+//     a {
+
+//     }
+
+//     b {
+
+//     }
 // };
-// console.log(obj.name);
-// console.log(obj.courses);
-
-// //slice,splice
 
 
+// let p1 = new Promise((resolve, reject)=>{
+//     //  setTimeout(()=>{
+//     //     resolve();
+//     //  },4000)
 
-// // var a=10
-// // console.log(a,typeof(a));
-// // console.log(a,typeof(a),typeof(typeof(a)));
+//     setTimeout(()=>{
+//         reject();
+//     }, 4000)
+// });
 
-// // // var b=String(a)
-// // // console.log(b,typeof(b));
+// console.log(p1);
 
-// // var name="poll"
-
-// // // console.log(`welcome to bengal coding acdemy ${name},hoe are you ${name}`);
-
-// // console.log(0/0);
-// // console.log(undefined);
-// // console.log(typeof(null));
-// // console.log(typeof(NaN));
-// // console.log(isNaN(100));
-// // console.log(isNaN("100d"));
-// // console.log(isNaN("d"));
-
-// let str="bengal coding academy";
-// console.log(str.charAt(str.length));
-// console.log(str.charAt(str.length -1));
-// // "length" => this is attribute
-
-// let a="hello "
-// let b="world"
-// // let c=a.concat(b);
-// let c=b.concat(a);
-// console.log(c);
-
-// console.log(a.indexOf("l"));
-// console.log(a.lastIndexOf("l"));
-// // array
-//  let arr=[10,40,[9,8,6],56,"hlw world",true,false]
-// // console.log(arr[4]);
-// // console.log(arr[2][1]);
-
-// // 2d array
-// let arr1=[[1, 2, 3],
-//           [4, 5, 6],
-//           [7, 8, 9]];
-
-//  console.log(arr1[1][1]);
-//  console.log(arr1[2][2]);
-
-// // 2.object
-// let obj={
-//     name: "bca",
-//     age:2,
-//     courses:["fswd","c","dsa"],
-//     location:"kolcation",
-//     isVirtual: true,
-// };
-// console.log(obj.name);
-// console.log(obj.courses);
-
-// //slice,splice
-
-
-// let a=18<20
-// let b=18>20
-
-// console.log(a);
-// console.log(b);
-// let b=76;
-// c=56;
-// console.log(c>b);
-
-
-// if(899){
-//     console.log("i am from if block");
-    
-
+// function successCallBack(){
+//     console.log("Success");
 // }
-// else{
-//     console.log("i am from else block");
-    
+// function successCallBack1(){
+//     console.log("Successsss1111");
+// }
+// function successCallBack2(){
+//     console.log("Successsss2222");
 // }
 
-
-// -----for loop------
-// let a=5;
-// while(a>=0){
-// console.log(a);
-// a=a-1;
-
+// function errorCallBack(){
+//     console.log("Errorrrrrr");
+// }
+// function errorCallBack1(){
+//     console.log("Errorrrrr1111");
+// }
+// function errorCallBack2(){
+//     console.log("Errorrrrr2222");
 // }
 
-// -----------function-------------------
-// function callMe(){
-//     console.log("11");
-//     console.log("21");
-//     console.log("31");
-//     console.log("41");
-    
-//}
-
-// ----------------anonimus function-------------------------
-
-
-// let callMeplease = function(){
-//                              console.log("11");
-//                             //  console.log("21");
-//                             //  console.log("31");
-//                             //  console.log("41");
-//                             }
-    
-//                             //this is call function calling
-//  callMeplease();
-// callMeplease();
-// callMeplease();
-// callMe(); 
-
-// arrow function
-// let callMeplease = (a)=>{
-//     console.log(a);
-   
-// }
-// function hello(a){
-// console.log(a);
-
-// }
-// let a= "hello"
-   //this is call function calling
-// hello(a);
-// callMeplease(a);
-// callMeplease();
-// callMe(); 
-
-//----------------- higher order function-------------------
-
-// 1 number
-// function f1(){
-//     console.log("i am f1 function");
-    
-// }
-// function f2(abc){
-//     console.log("i am f2 function");
-//   abc();
-    
-// }
-// f2(f1);
-
-// --2 number example--
-
-// function f1(bca){
-//     console.log("i am f1 function");
-//     bca();
-// }
-// function f2(abc){
-//     console.log("i am f2 function");
-//     let newFunc=() => {
-//         console.log("carry me"); 
-//     }
-//   abc(newFunc);
-    
-// }
-// f2(f1);
-// i am f2 function
-//  i am f1 function
-//  carry me
-
-
-// find smaller
-// let a=20;
-// let b=9;
-// (function findSmaller(){
-//     // if(a<b){
-//     a > b ? console.log("a is bigger") : console.log("b is smaller");
-    
-// })()
-        
-        
-    // }
-    // else if()
-    // else{
-    //     console.log("b is smaller");
-        
-    // }
-
-
-
-
-    // -----------------------Hoisting-------------------------
-//  example 1
-
-//  console.log(a, b);
-
-//  let a = 20;
-
-// // console.log(a,b);
-
-//  const b=30;
-
-//  console.log(a,b);
- 
-// example 2
-
-// let a=20;
-// const b=30;
-// b=40;
-// console.log(a + b);
-
-
-// let a=20;
-// let b=30;
-// b=40;
-// console.log(a + b);
-
-//example 3
-
-// let a=20;
-// console.log(a + b);
-// let b=30;
-
-
-// let a=20;
-// console.log(a + b);
-// b=30;
-
-// example 4 using node.js
-
-// console.log("falguni ");
-// let a=20;
-// let b=40
-// console.log(a+b);
-
-// example 5 using node.js
-// let a=20;
-// function callMe(){
-//     let a=30; 
-//     console.log(a,b);
-// }
-// let b=90;
-// callMe();
-
-
-// example 6 using node.js
-// function callMe(){
-//     let a=1;
-//     let x=function(){
-//         console.log(2*a);
-        
-//     }
-//     x();
-// }
-// let a=78;
-// callMe()
-
-
-// example 7 using node.js
-// function callMe(){
-    
-//     let x=function(){
-//         console.log(2*a);
-        
-//     }
-//     x();
-// }
-// let a =78;
-// callMe()
-
-
-// example 8 using node.js
-// let obj1={
-//     name:"bengal coding acadamy",
-//     age:"3",
-//     location:"west brngal"
-// }
-// let obj2=obj1;
-// obj2.location="india";
-// console.log(obj1.location,obj2.location);
-
-// example 9 using node.js
-// return sum a and b
-// let a=40;
-// let b=30;
-// c=a+b;
-// console.log(c);
-
-// function sum (a,b){
-//     let c=a+b;
-//     return c;
-// }
-// // let result=sum(10,20)
-// // console.log(result);
-
-
-// console.log(sum(10,90))
-
-
-
-// example 10 using node.js(anonimous function)
-//  let sum=function(a,b){
-// let c=a+b;
-// return c;
-// }
-// console.log(sum(10,90))
-
-
-// let sum=function(a,b){
-//     return a+b;
-//     }
-//     console.log(sum(10,90))
-    
-   
-// example 11 using node.js(arrow function)
-// 1 no
-// let sum= (a,b)=>{
-//         return a+b;
-//      }
-//     console.log(sum(10,90))
-
-    // 2
-    // let sum = (a, b) =>  a+b;
-    // console.log(sum(10,3));
-
-
-
-
-    // ----------------- Closures --------------
-    // 1 example 
-
-    // let a=20;
-    // function callMe(a){
-    //     let text=function(){
-    //         console.log(2 * a);
-            
-    //     };
-    //      return text;
-    // }
-    // callMe(3 * a)();
-
-    // 2 no example
-   
-    // let a=20;
-    // function callMe(){
-    //     let text=function(){
-    //         console.log(9 * a);
-            
-    //     };
-    //      return text;
-    // }
-    // // callMe(3 * a)();
-    // callMe(3*a)();
-
-        // 3 no example
-
-    // let a=20;
-    // function callMe(){
-    //     let text=function(){
-    //         // console.log(2 * a);
-            
-    //     };
-    //      return text;
-    // }
-    // let result=callMe();
-    // console.log(typeof(result));
-    // result();
-
-    // 4 no example
-
-    // let a=20;
-    // function callMe(a){
-    //     let test=function(){
-    //         let test2=function(){
-    //         console.log(2 * a);
-            
-    //     };
-    //      return test2;
-    // }call f3 and f4
-    //     return test;
-    // }
-    // callMe(3 * a)()();
-    
-
-
-    // -----------------higher order function---------------
-    // example 1
-    // let f2=function(){
-    //     console.log("hello everyone");
-        
-    // }
-    // let f1=function(f2){
-    //     f2();
-
-    // }
-    // f1(f2);
-
-    // ---example 2----
-    // let f1=function(f2,f3,f4)
-    // here f2,f3,f4 is a call back function 
-    // and f1 is a higher order function
-
-    // let f2=function(x1,x2){  
-    //     console.log("hello everyone1");
-    //     x1(x2);
-    // }
-    // let f3=function(y2){
-    //     console.log("hello everyone2"); 
-    //     y2();
-    // }
-    // let f4=function(){
-    //     console.log("hello everyone3");  
-    // }
-
-    // let f1=function(f2,f3,f4){
-    //     f2(f3,f4);
-
-    // }
-    // f1(f2,f3,f4);
-
-    // ----------------callMe function-------------------------
-    // function callMe(func){
-    //     var a=function(){
-    //         return "hello hii";
-
-    //     };
-    //     func();
-    //     return a;
-    // }
-    // function f1(){
-    //     console.log("i am a function");
-        
-    // }
-    // console.log(callMe(f1)());
-    // // console.log(a);
-
-    // ex 2
-    // {
-    //     var a=90;
-    // }
-    // console.log(a);
-    // {
-    //     let x=50;
-    //     const y=40;
-    // }
-    // console.log(x,y);
-    
-
-    // -------------Array-------------
-    // 1)foreach function ex 1
-
-//     let arr=[10,99,89,78,9];
-// arr.forEach((element)=>{
-// console.log(element);
-
-// })
-
-// --------------------------using callback function----------------
-
-// let arr=[10,99,89,78,9];
-// arr.forEach((cbfn));
-//     function cbfn(element){
-// console.log(element);
-
-// }
-
-//cbfn or call back function using arroe function
-//  example 1
-
-// let arr=[10,99,89,78,9]; 
-// // let cbfn = (element) => {
-// //     console.log(element);
-// // };
-// // arr.forEach(cbfn);
-// arr.forEach(element => console.log(element));
-
-// example 2(addin 2 of element)
-
-//  let arr=[10,99,89,78,9]; 
-// arr.forEach(element => console.log(element+5));
-
-// example 3(adding 5 of element and show the index of element)
-
-// let arr=[10,99,89,78,9]; 
-// arr.forEach((element,idx) => console.log(element+5,"--->",idx));
-
-// example 4(adding 5 of element and show the index of element and array)
-
-// let arr=[10,99,89,78,9]; 
-// arr.forEach((element,idx,arr) => console.log(element+5,"--->",idx,"------>",arr));
-
-// using map function() and adding 10 current element
-
-// let arr=[10,99,89,78,9]; 
-// let graceNumber =arr.map((ele)=>{
-//     return ele +10;
-// })
-// console.log(graceNumber);
-
-
-// using filteredarray
-    
-//   let arr=[10,32,67,54,78,52,50]; 
-// let filteredarray =arr.filter((ele)=>{ return ele >50})
-// console.log(filteredarray);
-
-    //  ----orrr---
-   
-    let arr=[10,32,67,54,78,52,50]; 
-    let filteredarray =arr.filter(ele=>ele >=50)
-    console.log(filteredarray);  
-         
+// p1.then(successCallBack).then(successCallBack1).then(successCallBack2);
+// p1.catch(errorCallBack).then(errorCallBack1).then(errorCallBack2);
 
+// let errProm = p1.catch(errorCallBack);
+// let errProm2 = errProm.then(errorCallBack1);
+// errProm2.then(errorCallBack2);
 
 
+// .then() & .catch()
+
+// GET https://api.postalpincode.in/pincode/{PINCODE}
+
+// let prom1 = fetch("https://api.postalpincode.in/pincode/713405");
+
+// prom1.then((response)=> response.json()).then(data=>console.log(data));
+
+
+
+
+const PINCODE = "713405"
+
+async function getData(pin){
+    let p1 = await fetch(`https://api.postalpincode.in/pincode/${PINCODE}`);
+
+    let p2 = await p1.json();
+
+    let arr = p2[0].PostOffice;
+
+    arr.forEach(element => {
+        console.log(element.Name + " ===> " + element.District)
+    });
+
+}
+
+getData(PINCODE);
